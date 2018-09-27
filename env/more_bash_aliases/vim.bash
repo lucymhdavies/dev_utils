9f5b65c1d8b0475195e09b5ef09c2eba627dc433
Lucy Davinhart <lucy.davies@skybettingandgaming.com>
2018-09-27 11:51:55 +0100

    Modularise my massive bash aliases file

---
+++ env/more_bash_aliases/vim.bash
@@ -0,0 +1,14 @@
+#
+# vim and vim accessories
+#
+
+alias vi="vim -p"
+alias vim="vim -p"
+alias view="vim -R"
+export EDITOR=vim
+alias vibash="vim ~/.bashrc"
+alias vialiases="vim ~/.bash_aliases; realias"; alias vialias=vialiases
+# because when you use vim a lot, you expect :q to be quit
+alias :q="exit"
+alias :Q="exit"
+alias :wq="exit"
