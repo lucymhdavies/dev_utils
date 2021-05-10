dafa27ca3ea4f1624e2f04eea89974e7121e24ab
Lucy Davinhart <lucy@lucymhdavies.com>
2021-05-10 21:28:12 +0100

    Remove legacy LMHD cert

--- bork/002_lmhd.sh
+++ bork/002_lmhd.sh
@@ -10,10 +10,6 @@
 ok check "security dump-trust-settings -s -d | grep lmhd.me"
 if check_failed && satisfying; then
 	# If not, download and trust
-	ok download /tmp/lmhd_root_old.pem https://vault.lmhd.me/v1/pki_root/ca/pem
-	ok check "sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain /tmp/lmhd_root_old.pem"
-
-	# If not, download and trust
 	ok download /tmp/lmhd_root.pem https://vault.lmhd.me/v1/pki/root/ca/pem
 	ok check "sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain /tmp/lmhd_root.pem"
 fi
