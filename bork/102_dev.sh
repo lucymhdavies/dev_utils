9c01edf27b2029df163e18d2f2ddd5301d67329d
Lucy Davinhart <lucy@lucymhdavies.com>
2021-09-13 14:17:11 +0100

    Hashi Stuff

--- bork/102_dev.sh
+++ bork/102_dev.sh
@@ -20,15 +20,6 @@ ok gem jekyll
 # TODO: Staticli
 # https://github.com/staticli/staticli/issues/60
 
-ok brew-tap hashicorp/tap
-ok brew terraform-ls
-ok brew terraform
-
 # Cobra!
 ok check "go get github.com/spf13/cobra/cobra"
-
-
-# https://github.com/hashicorp/vault/issues/10645
-# ok brew vault
-# TODO: At least check for its existence for now
 
---
+++ bork/201_hashi.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bork satisfy
+# BORK_CONTEXT: EVERYWHERE
+
+
+# Terraform
+
+ok brew-tap hashicorp/tap
+ok brew terraform-ls
+ok brew terraform
+
+# Vault
+
+ok brew vault
+
+
