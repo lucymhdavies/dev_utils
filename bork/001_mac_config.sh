e6c94b4f6f9932485747c5c1dc39ac0f5b896c2e
Lucy Davinhart <lucy@lucymhdavies.com>
2021-09-13 18:43:04 +0100

    Dock and screensaver config

--- bork/001_mac_config.sh
+++ bork/001_mac_config.sh
@@ -20,4 +20,22 @@ ok defaults com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag bool true
 # Security
 #
 
-# TODO: Screensaver, prompt for password immediately
+# ask for password immediately after starting screensaver or sleep
+ok defaults com.apple.screensaver askForPassword bool true
+ok defaults com.apple.screensaver askForPasswordDelay bool false
+
+
+
+
+#
+# Dock
+#
+
+bork_any_updated=0
+
+# set icon size
+ok defaults com.apple.dock autohide bool true
+
+if any_updated; then
+  killall Dock
+fi
