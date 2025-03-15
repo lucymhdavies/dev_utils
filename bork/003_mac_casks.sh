5d5374d464339e0c7c4815f78b3c0b8c5c202ef6
Lucy Davinhart <lucy@lucymhdavies.com>
2025-03-15 19:35:28 +0000

    hacky config sync for Magnet

--- bork/003_mac_casks.sh
+++ bork/003_mac_casks.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bork satisfy
 # BORK_CONTEXT: EVERYWHERE
+DEV_UTILS_DIR=$HOME/git_src/dev_utils
 
 #
 # Casks
@@ -37,6 +38,28 @@ ok defaults com.apple.WindowManager EnableTiledWindowMargins bool false
 ok defaults com.apple.WindowManager EnableTilingByEdgeDrag bool false
 ok defaults com.apple.WindowManager EnableTilingOptionAccelerator bool false
 ok defaults com.apple.WindowManager EnableTopTilingByEdgeDrag bool false
+
+# Rather than trying to check each parameter one at a time... here's just a known-good config file
+ok check "diff -q ${DEV_UTILS_DIR}/defaults/com.crowdcafe.windowmagnet <(defaults export com.crowdcafe.windowmagnet -)"
+# TODO: there is an issue related to what I think is Magnet syncing config to iCloud...
+	# $ diff ${DEV_UTILS_DIR}/defaults/com.crowdcafe.windowmagnet <(defaults export com.crowdcafe.windowmagnet -)
+	#331a332,335
+	#>       <key>lastPreferenceSync</key>
+	#>       <data>
+	#>       NzYxMzQ3Nzg4LjY3NTYxMw==
+	#>       </data>
+# so we need to ignore that somehow...
+# (but even then... iCloud syncing is disabled, because it doesn't work on the MacBook)
+
+if check_failed; then
+	echo "Magnet defaults do not match dev_utils"
+	diff ${DEV_UTILS_DIR}/defaults/com.crowdcafe.windowmagnet <(defaults export com.crowdcafe.windowmagnet -)
+
+	if satisfying; then
+		echo "Setting Magnet defaults"
+		defaults import com.crowdcafe.windowmagnet ${DEV_UTILS_DIR}/defaults/com.crowdcafe.windowmagnet
+	fi
+fi
 
 
 # TODO: System Preferences > Accessibility > Magnet.app
