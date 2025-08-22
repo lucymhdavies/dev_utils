4b1f786e410e49b51e155b393377787da32d4e65
Lucy Davinhart <lucy@lucymhdavies.com>
2025-08-22 19:48:56 +0100

    remove some dev tools we no longer use

--- bork/102_dev.sh
+++ bork/102_dev.sh
@@ -4,22 +4,3 @@
 ok mas 497799835 Xcode
 did_install && xcode-select --install
 did_install && sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer/
-
-
-
-#
-# Some useful Ruby Gems
-#
-
-ok brew ruby
-ok gem bundler
-ok gem jekyll
-
-
-
-# TODO: Staticli
-# https://github.com/staticli/staticli/issues/60
-
-# Cobra!
-ok check "go install github.com/spf13/cobra-cli@latest"
-
